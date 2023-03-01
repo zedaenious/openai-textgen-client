35955da4692f5014832727ca9aa0b5949cf0188b
zedaenious <zed.dev.22@gmail.com>
2023-03-01 16:42:59 -0500

    production domain

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5170', {
+  const response = await fetch('https://openai-textgen-server.vercel.app/api/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
